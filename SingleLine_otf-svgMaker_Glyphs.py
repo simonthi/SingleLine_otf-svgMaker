2b0b8d284f5125b1573d6173f2fbe4b0eaaf8dd9
simonthi <github@simonthiefes.de>
2024-06-10 16:00:11 +0200

    Made the script slightly smoother.
    * SVG Instance only gets attached, when it does not exist. Script proceeds syncing layers.
    * Layers only get synced and not created if they already exist.
    * Isolated functions.
    * Error messages are more human readable.

--- SingleLine_otf-svgMaker_Glyphs.py
+++ SingleLine_otf-svgMaker_Glyphs.py
@@ -12,50 +12,64 @@ strokeWeight = 40
 #load font
 font = Glyphs.font
 
-#check for existant svg instance
-instanceNames = [instance.name for instance in font.instances]
-if ("SVG" in instanceNames):
-    print("SVG Instance already exists. Rename instance and run script again to proceed.")
+def appendInstance():
+	#check for existant svg instance
+	instanceNames = [instance.name for instance in font.instances]
+	if ("SVG" in instanceNames):
+		print("SVG Instance already exists. Only syncing layers.")
+		appendSVGLayer()
 
-#else: append SVG instance to font
-else:
-    # generate new SVG Instance of the fon
-    SVGinstance = GSInstance()
-    SVGinstance.name = "SVG"
-    # set tables in created instance
-    SVGinstance.customParameters['Color Layers to SVG'] = True
-    SVGinstance.customParameters['Export SVG Table'] = True
-    font.instances.append(SVGinstance)
-    #loop over glyphs
-    for glyph in Glyphs.font.glyphs:
-        #make changes non destructive
-        glyph.beginUndo()
-        #get content of bottommost layer 
-        layer = glyph.layers[0]
-        #create new svg layer
-        newLayer = GSLayer()
-        newLayer.name = 'svg'
-        newLayer.attributes["color"] = True
-        glyph.layers.append(newLayer)
-        #copy shapes and metrics
-        newLayerID = glyph.layers[newLayer.layerId]
-        newLayerID.shapes = copy.copy(layer.shapes)
-        newLayerID.LSB = layer.LSB
-        newLayerID.RSB = layer.RSB
-        #set paths to specified stroke weight
-        for path in newLayerID.paths:
-            #set stroke width/height
-            path.attributes['strokeWidth'] = strokeWeight
-            path.attributes['strokeHeight'] = strokeWeight
-            #set strokes to black color
-            path.attributes['strokeColor'] = NSColor.blackColor()
-        for path in layer.paths:
-            #set stroke width/height
-            path.attributes['strokeWidth'] = strokeWeight
-            path.attributes['strokeHeight'] = strokeWeight
-            #this should set the linecaps to rounded. however, it is ignored as of now. consider this a stub for future updates of glyphs
-            path.attributes['lineCapEnd'] = 3
-            path.attributes['lineCapStart'] = 3
-        #end undo cycles
-        glyph.endUndo()
-        Glyphs.redraw()
+	#else: append SVG instance to font
+	else:
+		print("SVG Instance created.")
+		# generate new SVG Instance of the fon
+		SVGinstance = GSInstance()
+		SVGinstance.name = "SVG"
+		# set tables in created instance
+		SVGinstance.customParameters['Color Layers to SVG'] = True
+		SVGinstance.customParameters['Export SVG Table'] = True
+		font.instances.append(SVGinstance)
+		appendSVGLayer()
+
+
+def appendSVGLayer():
+	#loop over glyphs
+	for glyph in Glyphs.font.glyphs:
+		hasColorLayer = [ly.attributes["color"] == True for ly in glyph.layers]
+		if(True in hasColorLayer):
+			continue
+		else:
+			#make changes non destructive
+			glyph.beginUndo()
+			#get content of bottommost layer 
+			layer = glyph.layers[0]
+			#create new svg layer
+			newLayer = GSLayer()
+			newLayer.name = 'svg'
+			newLayer.attributes["color"] = True
+			glyph.layers.append(newLayer)
+			#copy shapes and metrics
+			newLayerID = glyph.layers[newLayer.layerId]
+			newLayerID.shapes = copy.copy(layer.shapes)
+			newLayerID.LSB = layer.LSB
+			newLayerID.RSB = layer.RSB
+			#set paths to specified stroke weight
+			for path in newLayerID.paths:
+				#set stroke width/height
+				path.attributes['strokeWidth'] = strokeWeight
+				path.attributes['strokeHeight'] = strokeWeight
+				#set strokes to black color
+				path.attributes['strokeColor'] = NSColor.blackColor()
+			for path in layer.paths:
+				#set stroke width/height
+				path.attributes['strokeWidth'] = strokeWeight
+				path.attributes['strokeHeight'] = strokeWeight
+				#this should set the linecaps to rounded. however, it is ignored as of now. consider this a stub for future updates of glyphs
+				path.attributes['lineCapEnd'] = 3
+				path.attributes['lineCapStart'] = 3
+			#end undo cycles
+			glyph.endUndo()
+			Glyphs.redraw()
+			
+			
+appendInstance()
